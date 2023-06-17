45259a12a86cf497e19290f11909426a47984fdf
Merzouk MENHOUR <merzouk@gmail.com>
2023-06-17 17:59:56 +0200

    modifs

--- src/main/java/org/com/customer/tools/Tools.java
+++ src/main/java/org/com/customer/tools/Tools.java
@@ -3,12 +3,13 @@ package org.com.customer.tools;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class Tools {
-
+public final class Tools
+{
     private static final String regex = "^[a-zA-Z0-9_!#$%&amp;'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    private static final Pattern pattern = Pattern.compile(regex);
 
-    public static final boolean validateEmail(String emailValue){
-        Pattern pattern = Pattern.compile(regex);
+    public static boolean validateEmail(String emailValue)
+    {
         Matcher matcher = pattern.matcher(emailValue);
         return matcher.matches();
     }
